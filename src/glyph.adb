6df299341f837fe1bbddbc3808c8976bba904854
Lokesh <thirstymelon.01@gmail.com>
2026-07-24 21:49:22 +0530

    First code commit with root Glyph package

---
+++ src/glyph.adb
@@ -0,0 +1,2 @@
+package body Glyph is
+end Glyph;
---
+++ src/glyph.ads
@@ -0,0 +1,19 @@
+------------------------------------------------------------------------------
+--  Glyph
+--
+--  Root package of the Glyph embedded graphics framework.
+--
+--  Glyph is a portable framebuffer-based graphics library written in Ada for
+--  embedded systems. The library provides reusable graphics primitives,
+--  display drivers, font rendering, image rendering, and widgets while
+--  remaining independent of any specific microcontroller or display.
+--
+--  Initial development targets the RP2040 and SSD1306 OLED displays, but the
+--  architecture is designed to support additional hardware in the future.
+--
+--  Copyright (C) 2026
+------------------------------------------------------------------------------
+
+package Glyph is
+   pragma Pure;
+end Glyph;
